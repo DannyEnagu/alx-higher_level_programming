226154936bfaca7c46c5623cd295a8170f1d12ff
dannyEnagu <danielEnagu@gmail.com>
2022-12-04 08:21:35 +0000

    Program checks linked list for palindrom

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,100 @@
+#include"lists.h"
+
+listint_t *copy_list(listint_t **head);
+listint_t *reverse_list(listint_t **list);
+
+/**
+* is_palindrome - checks if a singly linked list is a palindrome
+*	(i.e reading values from left to right is same as reading
+*	from right to left)
+*
+* @head: head of the list which contains the entire linked list
+*
+* Return: 0 if list is not a palindrome, 1 if it is a palindrome
+*/
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *reversed, *current;
+
+	if (head == NULL)
+		return (0);
+
+	reversed = reverse_list(head);
+	current = *head;
+
+	while (current != NULL && reversed != NULL)
+	{
+		if (current->n != reversed->n)
+			return (0);
+
+		current = current->next;
+		reversed = reversed->next;
+	}
+
+	return (1);
+}
+
+/**
+* reverse_list - reverses a single linked list
+*
+* @list: list to reverse
+*
+* Return: A reversed linked list, NULL if fails
+*/
+
+listint_t *reverse_list(listint_t **list)
+{
+	listint_t *previous, *current, *next;
+	listint_t *reversed;
+
+	if (list == NULL)
+		return (NULL);
+	previous = NULL;
+	current = copy_list(list); /* return a copy of the linked list */
+	next = NULL;
+
+	while (current != NULL)
+	{
+	/* save the link to the next node before moving on */
+		next = current->next;
+	/* current next now points to the previous node */
+		current->next = previous;
+	/* previous now holds the reversed list */
+		previous = current;
+	/* continue loop*/
+		current = next;
+	}
+	reversed = previous;
+
+	return (reversed);
+}
+
+/**
+* copy_list - Create a copy of the original list
+*
+* @head: contains list to copy
+*
+* Return: A copy of the original list on success,
+*	NULL if fails
+*/
+
+listint_t *copy_list(listint_t **head)
+{
+	listint_t *current, *new_list;
+
+	if (head == NULL)
+		return (NULL);
+
+	current = *head; /* used for iterating list*/
+	new_list = NULL; /* head of the new list*/
+
+	while (current != NULL)
+	{
+	/* copy / past each node data to the end of new_list */
+		add_nodeint_end(&new_list, current->n);
+		current = current->next;
+	}
+
+	return (new_list);
+}
