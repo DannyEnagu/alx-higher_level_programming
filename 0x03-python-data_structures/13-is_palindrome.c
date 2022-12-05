fb535865214c940f7aa6ce9031663a9d598400cc
dannyEnagu <danielEnagu@gmail.com>
2022-12-05 11:03:11 +0000

    More code text

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -51,8 +51,8 @@ int is_palindrome(listint_t **head)
 	for (i = 0; i < (size / 2) - 1; i++)
 		tmp = tmp->next;
 
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);
+	/*if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0); */
 
 	tmp = tmp->next->next;
 	rev = reverse_listint(&tmp);
