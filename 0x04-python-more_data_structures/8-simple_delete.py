84452a776d70aa7c820ee72ef086df261a12322f
dannyEnagu <danielEnagu@gmail.com>
2022-12-07 15:28:48 +0000

    Python data structures and manipulatiins

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,17 @@
+#!/urs/bin/python3
+
+
+def simple_delete(a_dictionary, key=""):
+    """Deletes the provided key/value from a dictionary
+
+       Args:
+        @a_dictionary: python dictionary
+        @key: string argument
+
+       Return:
+        A new dictionary
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+
+    return a_dictionary
