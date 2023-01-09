81125baead3d93d215aff937ed3c5511f9e44da7
dannyEnagu <danielEnagu@gmail.com>
2023-01-09 18:54:15 +0000

    completed all tasks

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a module ``100-my_Int``"""
+
+
+class MyInt(int):
+    """Defines a new class ``MyInt`` is a rebel as it 
+       Invert int operators == and !=.
+    """
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
