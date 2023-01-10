1c1cb8ce18cb67761db424e4b552530054458cd1
dannyEnagu <danielEnagu@gmail.com>
2023-01-10 10:13:29 +0000

    File manipulation: reading/writing to files

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the ``1-write_file`` module which contains
+    the write_file function
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns
+        the number of characters written
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        data = f.write(str(text))
+        return (data)
