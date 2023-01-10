a40bb7d468b30244a3c076ac06e3b15e1acde9e2
dannyEnagu <danielEnagu@gmail.com>
2023-01-10 10:22:00 +0000

    File object manipulation: added comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,11 @@
 
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
+    """Reads a text file (UTF8) and prints it to stdout.
+
+       Args:
+        filename: path to file being read.
+    """
     with open(filename, encoding="utf-8") as f:
         data = f.read()
         print(data)
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,6 +7,13 @@
 def write_file(filename="", text=""):
     """Writes a string to a text file (UTF8) and returns
         the number of characters written
+
+       Args:
+        filename: path to file to write to.
+        text: string data being written.
+
+       Return:
+        number of characters written.
     """
     with open(filename, mode="w", encoding="utf-8") as f:
         data = f.write(str(text))
