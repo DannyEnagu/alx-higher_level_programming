5bcd453769d5580d9143e3f7563890a8746c31e4
dannyEnagu <danielEnagu@gmail.com>
2023-03-13 21:51:32 +0000

    Pinting a Square with loops

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const argv = process.argv;
+const squareSize = Number(argv[2]);
+if (squareSize) {
+  for (let i = 0; i < squareSize; i++) {
+    let row = '';
+    for (let j = 0; j < squareSize; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
