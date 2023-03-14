4c4d5f049e59e1cc9f53c420ce95e150c023bfb6
dannyEnagu <danielEnagu@gmail.com>
2023-03-14 15:20:53 +0000

    Update to try n fix code fail

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 
-const Square = class Squares extends Rectangle {
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-};
+}
 module.exports = Square;
