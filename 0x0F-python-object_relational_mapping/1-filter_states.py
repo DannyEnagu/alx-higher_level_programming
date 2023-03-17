f716042fd6dee955a73b14add73867115463847e
dannyEnagu <danielEnagu@gmail.com>
2023-03-17 07:34:18 +0000

    Updates

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,13 +16,13 @@ def main(argv):
                          passwd=argv[1],
                          db=argv[2], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY \
-                 id ASC", ('N%',))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", ('N%',))
     states = cur.fetchall()
     for state in states:
         print(state)
     cur.close()
     db.close()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
