21ad950b81d355537252551fad8571e1b4a27f59
DannyEnagu <danielenagu@gmail.com>
2023-04-03 13:24:17 -0700

    Change import to urllib

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,8 +2,7 @@
 """Fetches https://alx-intranet.hbtn.io/status
    using pythons urllib package.
 """
-import urllib.request
-import urllib.parse
+from urllib import request, parse
 import sys
 
 
@@ -11,9 +10,9 @@ if __name__ == "__main__":
     url = sys.argv[1]
     params = {}
     params['email'] = sys.argv[2]
-    data = urllib.parse.urlencode(params)
+    data = parse.urlencode(params)
     data = data.encode('ascii')  # data should be bytes
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
         data = response.read()
         print(f"Your email is: {data.decode()}")
