767e7d9b9076a71dca98979ff3f34836b6f65839
dannyEnagu <danielEnagu@gmail.com>
2023-04-26 00:24:36 +0000

    Update 1-writeme file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,15 +4,10 @@ const fs = require('fs');
 
 function writeToFile (filename, data) {
   fs.writeFile(filename, data, 'utf8', (err) => {
-    try {
-      if (err) throw err;
-    } catch (error) {
-      console.log(error);
-    }
+    if (err) console.log(err);
   });
 }
 
 if (argv.length > 3) {
-  const fileContent = `${argv[3]}\n`;
-  writeToFile(argv[2], fileContent);
+  writeToFile(argv[2], argv[3]);
 }
