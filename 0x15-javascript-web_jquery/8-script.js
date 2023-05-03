607c90b1909eed95aee04d0b1a2fe8c9d7bdc092
Daniel Enagu <daniel@cygnusspin.com>
2023-05-03 23:55:01 +0100

    8. Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+$(function()
+{
+  const url = 'https://swapi-api.alx-tools.com/api/films/?format=json'
+  $.get(url, function (films, textStatus)
+  {
+    $.each(films.results, function (i, film)
+    {
+       $('UL#list_movies').append('<li>'+ film.title +'</li>');
+    });
+  });
+});
